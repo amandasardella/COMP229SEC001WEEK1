4c0d316a139b5f2e6412208e9880cb0198b516a2
Amanda Sardella <amandansardella@gmail.com>
2023-05-18 17:17:47 -0300

    Lab 1 GitHub Demo Week 1

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-/* EXPLANATION EX. IMPORT AND EXPORT
-
+/*
 import { halfOf, multiply } from "./lib.js";
 console.log(halfOf(84));
-console.log(multiply(21, 2));
+console.log(multiply(22, 2));
 */
 
-/* NORMAL FUNCTION
+/*
+function sum1(a, b) {
+  return a + b;
+}
+let result = sum1(1, 2);
+console.log(result);
+*/
 
+//NORMAL FUNCTION
+/*
 function sum1(a, b) {
     return a + b;
 }
@@ -14,8 +21,8 @@ let result = sum1(1, 2);
 console.log(result);
 */
 
-/* ARROW FUNCTION
-
+//ARROW FUNCTION
+/*
 const sum1 = (a, b) => {
     return a + b;
 }
@@ -23,8 +30,59 @@ let result = sum1(1, 2);
 console.log(result);
 */
 
-/* OR (ARROW FUNCTION SHORTER) 
+//OR (ARROW FUNCTION SHORTER)
+/*
 const sum1 = (a, b) => a + b;
 let result = sum1(1, 2);
 console.log(result);
+*/
+
+//LET AND CONST
+/*
+function reinitConst() {
+  let me = 1;
+  console.log(me);
+  me = 2;
+  console.log(me);
+}
+reinitConst();
+*/
+
+//DEFAULT VALUE
+/*
+function add(x, y = 0) {
+  console.log(x + y);
+}
+add(1);
+add(1, 2);
+*/
+
+//REST
+/*
+function userFriends(user, ...friends) {
+  console.log(user + " has " + friends.length + " friends");
+}
+userFriends("User", "Bob", "Alice", "Cal");
+*/
+
+//SPREAD - turns array into call argument
+/*
+function userTopFriends(firstFriend, secondFriend, thirdFriends) {
+  console.log(firstFriend);
+  console.log(secondFriend);
+  console.log(thirdFriends);
+}
+userTopFriends(...["Alice", "Bob", "Michelle"]);
+*/
+
+//CLOSURES
+/*
+function parent() {
+  const message = "Hello World";
+  function child() {
+    alert(message);
+  }
+  child();
+}
+parent();
 */
