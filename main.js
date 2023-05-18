7e2a97859745933e6811089441c6f95476c5d703
Amanda Sardella <amandansardella@gmail.com>
2023-05-18 17:01:57 -0300

    Week 1 Lab 1 - GitHub Repo

---
+++ main.js
@@ -0,0 +1,30 @@
+/* EXPLANATION EX. IMPORT AND EXPORT
+
+import { halfOf, multiply } from "./lib.js";
+console.log(halfOf(84));
+console.log(multiply(21, 2));
+*/
+
+/* NORMAL FUNCTION
+
+function sum1(a, b) {
+    return a + b;
+}
+let result = sum1(1, 2);
+console.log(result);
+*/
+
+/* ARROW FUNCTION
+
+const sum1 = (a, b) => {
+    return a + b;
+}
+let result = sum1(1, 2);
+console.log(result);
+*/
+
+/* OR (ARROW FUNCTION SHORTER) 
+const sum1 = (a, b) => a + b;
+let result = sum1(1, 2);
+console.log(result);
+*/
